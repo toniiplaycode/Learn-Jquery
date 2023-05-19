0554b04a888ad87c48329696b9ffdfd6548a9ada
THANH TOAN <toan13022017@gmail.com>
2023-05-19 15:28:36 +0700

    update

--- html.js
+++ html.js
@@ -28,4 +28,49 @@ $(document).ready(function(){
     $(".btn-set-val").click(function(){
         $(".set-val").val('hello');
     });
+
+    //----------------addpend--------------
+    $(".btn-append").click(function(){
+        $(".append").append("<span> add addpend </span>");
+    });
+
+    //----------------prepend--------------
+    $(".btn-prepend").click(function(){
+        $(".prepend").prepend("<span> add prepend </span>");
+    });
+
+    //----------------after--------------
+    $(".btn-after").click(function(){
+        $(".after").after("<p> add after </p>");
+    });
+
+    //----------------before--------------
+    $(".btn-before").click(function(){
+        $(".before").before("<p> add before </p>");
+    });
+
+    //----------------remove--------------
+    $(".btn-remove").click(function(){
+        $(".remove").remove();
+    });
+
+    //----------------empty--------------
+    $(".btn-empty").click(function(){
+        $(".empty").empty();
+    });
+
+    //----------------addClass--------------
+    $(".btn-addClass").click(function(){
+        $(".addClass-removeClass").addClass("color-red");
+    });
+
+    //----------------removeClass--------------
+    $(".btn-removeClass").click(function(){
+        $(".addClass-removeClass").removeClass("color-red");
+    });
+
+    //----------------toggleClass--------------
+    $(".btn-toggleClass").click(function(){
+        $(".toggleClass").toggleClass("color-red");
+    });
 }); 
